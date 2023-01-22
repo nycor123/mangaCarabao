e01f5124da4182ce9292479e11de4414b357ee37
Nycor Estrada <nycor123@gmail.com>
2023-01-22 21:57:44 +0800

    create reader footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Container } from 'react-bootstrap';
 import { Header } from './navigationComponents/Header';
+import { Footer } from './navigationComponents/Footer';
 import { Reader } from './Reader';
 import onePieceChapters from './OnePiece';
 
@@ -9,16 +10,28 @@ function App() {
     title: "One Piece",
     chapters: onePieceChapters
   });
-  const [chapter, setChapter] = useState("Ch1070");
-  const [pageUrls, setPageUrls] = useState(manga.chapters[chapter]);
+  const [chapter, setChapter] = useState("1071");
+
+  const toPrevChapter = (e) => {
+    setChapter(prev => parseInt(prev) - 1);
+  }
+
+  const toNextChapter = (e) => {
+    setChapter(prev => parseInt(prev) + 1);
+  }
 
   return (
     <Container>
       <Header />
       <Reader 
         manga={manga} 
-        chapter={chapter} 
-        pageUrls={pageUrls}
+        chapter={chapter}
+        pageUrls={manga.chapters[chapter]}
+      />
+      <Footer 
+        chapter={chapter}
+        toPrevChapter={toPrevChapter}
+        toNextChapter={toNextChapter}
       />
     </Container>
   );
